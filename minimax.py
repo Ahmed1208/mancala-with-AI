72851f941cafdb54cfdf64d1386d56ebe2e3b478
Ahmed Yehia <ahmedyehia.edu@gmail.com>
2021-06-11 13:29:45 +0200

    implement minimax function with alpha beta pruning

--- minimax.py
+++ minimax.py
@@ -1,3 +1,38 @@
+from Utilities import all_possibles
+
+def minimax(board, alpha, beta, depth, max_depth, next_step ,steal):
+    
+    if next_step == 0:
+        boards = all_possibles(board,"player" + str((depth % 2)+1),steal)
+    else:
+        boards = [[board, False]]
+
+    if depth == max_depth or len(boards) == 0:
+        res = evaluate(board)
+        if depth % 2 == 0:
+            return max(res, alpha), beta,0
+        else:
+            return alpha, min(res, beta),0
+
+    new_alpha = alpha
+    new_beta = beta
+    bs = []
+    for i in boards:
+        if new_alpha >= new_beta:
+            return beta,alpha,0
+        a,b,dummy = minimax(i[0], new_alpha, new_beta, depth+1, max_depth, i[1],steal)
+        if depth % 2 == 0:
+            new_alpha = max(new_alpha, b)
+            bs.append(b)
+        else:
+            new_beta = min(new_beta, a)
+            bs.append(b)
+    if depth % 2 == 0:
+        return new_alpha,new_beta,-max((x,-i) for i,x in enumerate(bs))[1]
+    else:
+        return new_alpha,new_beta,min((x,i) for i,x in enumerate(bs))[1]
+
+
 def evaluate(board):
     count1 = board[7]
     count2 = board[0]
